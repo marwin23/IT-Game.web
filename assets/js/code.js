e3d89b949d52fac41a2cd173282df09b5a2f490a
Marwin Freutel <marwin.freutel@mail.de>
2025-09-17 21:32:35 +0200

    set  parking zones. load menu.

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,16 +1,34 @@
 // IT-Game Code
 function activateDocument() {
     const g = document.getElementById("game");
-    const ctx = g.getContext("2d");
-    
+    const gctx = g.getContext("2d");  
+    const m = document.getElementById("menu");
+    const mctx = m.getContext("2d");
     const f = document.getElementById("field");
-    
+
     f.onload = function(e) {
-        ctx.canvas.width = f.width;
-        ctx.canvas.height = f.height;
-        ctx.drawImage(f, 0, 0, f.width,f.height);
+        gctx.canvas.width = f.width;
+        gctx.canvas.height = f.height;
+        gctx.drawImage(f, 0, 0, f.width,f.height);
+
+        mctx.canvas.width = f.width;
+        mctx.canvas.height = 20;
     };
+
+    const images = [ "new", "force", "jump", "ranking", "player", "sound" ];
+    for( var i = 0; i < images.length; i++) {
+        var icon = document.getElementById(images[i]);
+        icon.index = i;
+        icon.onload = function() {
+            mctx.drawImage( this, this.index * 20, 0, this.width, this.height);
+        }
+    }
 };
+
+// set parking zones on field
+function setParking(col,row) {
+
+}
 
 
 // --- end of file ---
